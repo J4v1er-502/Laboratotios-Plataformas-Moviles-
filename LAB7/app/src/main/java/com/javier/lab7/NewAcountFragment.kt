f60cbc881cdb953d1850f8f4ecdaa952a571aa06
Javier Alejandro Ramírez Heredia <ram21600@uvg.edu.gt>
2022-08-28 22:55:27 -0600

    LAB7

---
+++ LAB7/app/src/main/java/com/javier/lab7/NewAcountFragment.kt
@@ -0,0 +1,13 @@
+package com.javier.lab7
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class NewAcountFragment : Fragment(R.layout.fragment_new_acount) {
+
+}
---
+++ LAB7/app/src/main/java/com/javier/lab7/ProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.javier.lab7
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+}
